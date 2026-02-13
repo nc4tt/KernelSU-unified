6681db188482fdbd3f18ac586f22a2ebd7cdbaee
nc4tt <anntuongg@gmail.com>
2026-02-13 11:54:06 +0700

    more manager supported (pt2)

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -23,6 +23,17 @@
 #define EXPECTED_HASH_KOWX712                                                  \
 	"484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"
 
+// KernelSU-Next/KernelSU-Next
+#define EXPECTED_SIZE_NEXT 0x3e6
+#define EXPECTED_HASH_NEXT                                                  \
+	"79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
+
+// ShirkNeko/KernelSU
+#define EXPECTED_SIZE_SHIRKNEKO 0x35c
+#define EXPECTED_HASH_SHIRKNEKO                                                \
+	"947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef"
+
+
 typedef struct {
 	u32 size;
 	const char *sha256;
